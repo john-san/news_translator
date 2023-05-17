b3156562b226d0b35d8f8038dacc58894253f099
John Nguyen <npjohn92@gmail.com>
2023-05-16 20:18:54 -0700

    add looping question to increase card capacity

--- src/getVocab.ts
+++ src/getVocab.ts
@@ -2,68 +2,62 @@ import { ChatGPTAPI } from 'chatgpt'
 import dotenv from 'dotenv'
 dotenv.config()
 
-export default async function getVocab(content: string): Promise<Vocab[]> {
+export default async function getVocab(
+	content: string,
+	num: number = 20
+): Promise<Vocab[]> {
 	const api = new ChatGPTAPI({
 		apiKey: process.env.CHATGPT_API_KEY ?? '',
 	})
-	//const sampleContent =
-	// 'Thông tin được ông Lương Minh Phúc, Giám đốc Ban quản lý dự án đầu tư xây dựng các công trình giao thông TP HCM (TCIP - chủ đầu tư dự án xây lắp trên địa bàn thành phố), cho biết chiều 11/5, sau khi tổ công tác vật liệu liên tỉnh tuyến Vành đai 3 cung cấp số liệu. UBND thành phố là cơ quan điều phối chung dự án.'
-	let number = 20
-	console.log('Asking ChatGPT for vocab.')
-	let question = `From the following text, can you provide me with ${number} flashcards for important words to understand the text that contain: 1) The Vietnamese word, 2) the English translation, 3) root words : ${content}. Please give me the flashcards in an array of objects: [{"VN": "vietnamese word", "EN": "english translation", rootWords: "root word 1(translation), root ward 2(translation),..."},...]`
+	// if num is greater than 20, ask for 20 at a time until num is less than 20
+	let result: Vocab[] = []
+	let vocabArray: Vocab[] = []
 
-	let res = await api.sendMessage(question)
-	let vocabArray = parseVocabArray(res.text)
+	let CHUNK_SIZE = 20
+	let remaining = num
+	let parentMessageId: string | undefined = ''
 
-	// if vocabArray is empty, ask again
-	let counter = 1
-	while (vocabArray.length == 0 && counter < 6) {
-		await sleep(20)
-		console.log(
-			`Didn't get array of vocab. asking again. Re-attempt ${counter}`
-		)
-		// follow up question
-		question = `I didn't get an array of objects. Please try again. Please give me ${number} flashcards for key terms from the previous Vietnamese text in an array of objects: [{"VN": "vietnamese word", "EN": "english translation", "rootWords": "root word 1(translation), root ward 2(translation),..."},...]`
-		res = await api.sendMessage(question, {
-			parentMessageId: res.id,
-		})
+	while (remaining > 0) {
+		console.log(`Asking ChatGPT for ${remaining == num ? '' : 'more '}vocab.`)
+		let question = `From the following text, can you provide me with ${CHUNK_SIZE} ${
+			remaining == num ? '' : 'NEW(not given previously)'
+		} flashcards for important words to understand the text that contain: 1) The Vietnamese word, 2) the English translation, 3) root words : ${content}. Please give me the flashcards in an array of objects: [{"VN": "vietnamese word", "EN": "english translation", rootWords: "VN root word 1(EN translation), VN root ward 2(EN translation),..."},...]`
+
+		let res = await api.sendMessage(question, { parentMessageId })
 		vocabArray = parseVocabArray(res.text)
-		counter++
+
+		// if vocabArray is empty, ask again
+		let counter = 1
+		while (vocabArray.length == 0 && counter < 6) {
+			console.log(
+				`Didn't get array of vocab. Will ask again after 20 second cooldown. Retries left: ${
+					6 - counter
+				}`
+			)
+			await sleep(20)
+			// follow up question
+			question = `I didn't get an array of objects. Please try again. Please give me ${CHUNK_SIZE} ${
+				remaining == num ? '' : 'NEW(not given previously)'
+			} flashcards for key terms from the previous Vietnamese text in an array of objects: [{"VN": "vietnamese word", "EN": "english translation", "rootWords": "VN root word 1(EN translation), VN root ward 2(EN translation),..."},...]`
+			res = await api.sendMessage(question, {
+				parentMessageId: res.id,
+			})
+
+			parentMessageId = res.parentMessageId // set parentMessageId to the id of the last message for next iteration
+
+			vocabArray = parseVocabArray(res.text)
+			counter++
+		}
+
+		console.log('Successfully retrieved vocab for this iteration.')
+
+		// merge vocabArray with result
+		result = result.concat(vocabArray)
+		// decrement remaining by CHUNK_SIZE. If remaining is less than CHUNK_SIZE, it is on the last iteration, so set remaining to 0
+		remaining = remaining >= CHUNK_SIZE ? remaining - CHUNK_SIZE : 0
 	}
-
-	// if vocabArray is still empty, return empty array
-	if (vocabArray.length == 0) {
-		console.log('Still no vocab. Returning empty array.')
-		return []
-	}
-
-	// try to get example sentences
-	// counter = 1
-	// let sentenceArray: Vocab[] = []
-	// while (vocabArray[0].example == undefined && counter < 6) {
-	// 	await sleep(20)
-	// 	console.log(`Didn't get example sentences. Re-attempt ${counter}`)
-	// 	// follow up question
-	// 	question = `I didn't get any example sentences. Please try again. Please give me flashcards for key terms from the previous Vietnamese text in an array of objects: [{VN: "vietnamese word", example: "example sentence", original: "original sentence from text" },...]`
-	// 	res = await api.sendMessage(question, {
-	// 		parentMessageId: res.id,
-	// 	})
-	// 	sentenceArray = parseVocabArray(res.text)
-	// 	counter++
-	// }
-
-	// // merge vocabArray and sentenceArray
-	// if (sentenceArray.length > 0) {
-	// 	vocabArray = vocabArray.map((vocab, index) => {
-	// 		return {
-	// 			...vocab,
-	// 			example: sentenceArray[index].example,
-	// 			original: sentenceArray[index].original,
-	// 		}
-	// 	})
-	// }
-
-	return vocabArray
+	console.log('result.length: ', result.length)
+	return result
 }
 
 type Vocab = {
@@ -80,17 +74,17 @@ function parseVocabArray(text: string): Vocab[] {
 	let start = text.indexOf('[') // find index of first '['
 	let end = text.lastIndexOf(']') // find index of last ']'
 
-	console.log('text:', text)
-	console.log('start:', start)
-	console.log('end:', end)
+	// console.log('text:', text)
+	// console.log('start:', start)
+	// console.log('end:', end)
 
 	// if both '[' and ']' exist, parse json
 	if (start != -1 && end != -1) {
 		let json = text.substring(start, end + 1)
-		console.log('json:', json)
+		// console.log('json:', json)
 		try {
 			const parsedJson: Vocab[] = JSON.parse(json)
-			console.log('parsedJson:', parsedJson)
+			// console.log('parsedJson:', parsedJson)
 			return parsedJson
 		} catch (error) {
 			console.error('Error parsing JSON:', error)
