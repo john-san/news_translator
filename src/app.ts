fb56c6902ab3b87e3275d95b1509f62a7d264a69
John Nguyen <npjohn92@gmail.com>
2023-05-25 16:10:12 -0700

    add user input to pick topic > subtopic

--- src/app.ts
+++ src/app.ts
@@ -1,64 +1,171 @@
 // for VN Express
-import puppeteer from 'puppeteer'
-import getVocab from './getVocab'
-import getEasySentences from './getEasySentences'
-import { parseDate } from './utils'
-import writeToCsv from './writeToCsv'
-
-const url = 'https://vnexpress.net/topic/tp-ho-chi-minh-26483'
-// click the the div with class name: article-topstory
-// get the url of the article
-// get the h1 tag with class name: title-detail
-// get the article tag with class name: fck_detail
-// look for span tag with class name: date
-// save the contents to a csv file
+import puppeteer from "puppeteer";
+import readline from "readline";
+import getVocab from "./getVocab";
+import getEasySentences from "./getEasySentences";
+import { parseDate } from "./utils";
+import writeToCsv from "./writeToCsv";
 
 const scrape = async (): Promise<any> => {
-	try {
-		const browser = await puppeteer.launch({ headless: 'new' })
-		const page = await browser.newPage()
-		await page.goto(url)
-		await page.waitForSelector('.article-topstory')
-		await page.click('.article-topstory')
-		await page.waitForSelector('.title-detail')
-		await page.waitForSelector('.fck_detail')
-		await page.waitForSelector('.date')
-		const result = await page.evaluate(() => {
-			let url = document.location.href
-			let title = document.querySelector('.title-detail')?.textContent?.trim()
-			let content = document.querySelector('.fck_detail')?.textContent?.trim()
-			let date = document.querySelector('.date')?.textContent
-			// remove newlines and tabs from content
-			content = content?.replace(/\n|\t/g, '')
+  try {
+    let accepted = false;
 
-			return {
-				url,
-				title,
-				content,
-				date,
-			}
-		})
-		browser.close()
+    const browser = await puppeteer.launch({ headless: "new" });
+    const page = await browser.newPage();
+    const rl = readline.createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
+    let result: any = {};
+    // await page.goto(url)
 
-		return result
-	} catch (error) {
-		console.log(error)
-	}
-}
+    while (accepted == false) {
+      await page.goto("https://vnexpress.net");
+      await page.waitForSelector("nav.main-nav");
+
+      const topics = await page.evaluate(() => {
+        const topicLinks = Array.from(
+          document.querySelectorAll("nav.main-nav > ul.parent > li > a")
+        );
+        return topicLinks.map((link, index) => {
+          const anchor = link as HTMLAnchorElement;
+          return {
+            index: index + 1,
+            name: anchor.textContent!.trim(),
+            href: anchor.href,
+          };
+        });
+      });
+
+      console.log("Pick a topic:");
+      topics.forEach((topic) => {
+        console.log(`${topic.index}. ${topic.name}`);
+      });
+
+      const topicIndex = await new Promise<number>((resolve) => {
+        rl.question(
+          "Enter the number of the topic you want to explore: ",
+          (topicIndex) => {
+            resolve(parseInt(topicIndex, 10));
+          }
+        );
+      });
+
+      const selectedTopic = topics.find((topic) => topic.index === topicIndex);
+      if (!selectedTopic) {
+        console.log("Invalid topic number. Exiting...");
+        rl.close();
+        await browser.close();
+        return;
+      }
+
+      console.log(`You selected "${selectedTopic.name}".`);
+
+      await page.goto(selectedTopic.href);
+      await page.waitForSelector("ul.ul-nav-folder");
+
+      const subtopics = await page.evaluate(() => {
+        const subtopicLinks = Array.from(
+          document.querySelectorAll("ul.ul-nav-folder > li a")
+        );
+        return subtopicLinks.map((link, index) => {
+          const anchor = link as HTMLAnchorElement;
+          return {
+            index: index + 1,
+            name: anchor.textContent!.trim(),
+            href: anchor.href,
+          };
+        });
+      });
+
+      console.log("Pick a subtopic:");
+      subtopics.forEach((subtopic) => {
+        console.log(`${subtopic.index}. ${subtopic.name}`);
+      });
+
+      const subtopicIndex = await new Promise<number>((resolve) => {
+        rl.question(
+          "Enter the number of the subtopic you want to explore: ",
+          (subtopicIndex) => {
+            resolve(parseInt(subtopicIndex, 10));
+          }
+        );
+      });
+
+      const selectedSubtopic = subtopics.find(
+        (subtopic) => subtopic.index === subtopicIndex
+      );
+      if (!selectedSubtopic) {
+        console.log("Invalid subtopic number. Exiting...");
+        rl.close();
+        await browser.close();
+        return;
+      }
+
+      console.log(`You selected "${selectedSubtopic.name}".`);
+
+      await page.goto(selectedSubtopic.href);
+
+      await page.waitForSelector(".article-topstory");
+      await page.click(".article-topstory");
+      await page.waitForSelector(".title-detail");
+      await page.waitForSelector(".fck_detail");
+      await page.waitForSelector(".date");
+      result = await page.evaluate(() => {
+        let url = document.location.href;
+        let title = document
+          .querySelector(".title-detail")
+          ?.textContent?.trim();
+        let content = document
+          .querySelector(".fck_detail")
+          ?.textContent?.trim();
+        let date = document.querySelector(".date")?.textContent;
+        // remove newlines and tabs from content
+        content = content?.replace(/\n|\t/g, "");
+
+        return {
+          url,
+          title,
+          content,
+          date,
+        };
+      });
+
+      console.log("result:", result);
+      console.log("Do you want to study this article? (y/n)");
+      const answer = await new Promise<string>((resolve) => {
+        rl.question("Enter your answer: ", (answer) => {
+          resolve(answer);
+        });
+      });
+      if (answer === "y") {
+        console.log("Using the current article.");
+        accepted = true;
+      }
+    }
+
+    browser.close();
+    rl.close();
+
+    return result;
+  } catch (error) {
+    console.log(error);
+  }
+};
 
 async function scrapeAndProcess() {
-	const result = await scrape()
-	result.date = parseDate(result?.date)
+  const result = await scrape();
+  result.date = parseDate(result?.date);
 
-	console.log(result)
+  console.log(result);
 
-	result.vocab = await getVocab(result.content)
-	console.log(result.vocab)
+  result.vocab = await getVocab(result.content);
+  console.log(result.vocab);
 
-	result.sentences = await getEasySentences(result.content)
-	console.log(result.sentences)
+  result.sentences = await getEasySentences(result.content);
+  console.log(result.sentences);
 
-	await writeToCsv(result)
+  await writeToCsv(result);
 }
 
 /* TODO:
@@ -66,4 +173,4 @@ async function scrapeAndProcess() {
 - fix issue with npm start
 */
 
-scrapeAndProcess()
+scrapeAndProcess();
